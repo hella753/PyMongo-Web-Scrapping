0ebf09a9617a4e72f319788234d22203fa6f627e
hella753 <kristi.gaphrindashvili@gmail.com>
2024-09-25 22:53:27 +0400

    formatting

--- data_fetcher.py
+++ data_fetcher.py
@@ -3,23 +3,48 @@ import requests
 import asyncio
 from typing import List
 
+
 class DataFetcher:
+    """
+    Handles fetching data from the website using requests and
+    aiohttp. It uses a semaphore to limit the number of concurrent
+    requests to the website.
+    """
     def __init__(self) -> None:
         self.urls: List[str] = []
         self.semaphore: asyncio.Semaphore = asyncio.Semaphore(5)
 
     @staticmethod
     def fetch_data(url: str) -> str:
+        """
+        Fetches data from the website using requests.
+
+        :param url: str: URL of the website to fetch data from
+        :return: str: response from the website
+        """
         response = requests.get(url).text
         return response
 
     @staticmethod
-    async def fetch(session, url: str):
+    async def fetch_async(session: aiohttp.ClientSession, url: str) -> str:
+        """
+        Fetches data from the website using aiohttp.
+
+        :param session: aiohttp.ClientSession: aiohttp session object
+        :param url: str: URL of the website to fetch data from
+        :return: str: response from the website
+        """
         async with session.get(url) as response:
             return await response.text()
 
-    async def fetch_all(self, urls: List[str]):
+    async def fetch_async_all(self, urls: List[str]) -> List[str]:
+        """
+        Fetches data from multiple websites concurrently using aiohttp.
+
+        :param urls: List[str]: List of URLs of the websites to fetch data from
+        :return: List[str]: List of responses from the websites
+        """
         async with self.semaphore:
             async with aiohttp.ClientSession() as session:
-                tasks = [self.fetch(session, url) for url in urls]
+                tasks = [self.fetch_async(session, url) for url in urls]
                 return await asyncio.gather(*tasks)
--- recipe.py
+++ recipe.py
@@ -1,6 +1,9 @@
 
 
 class Recipe:
+    """
+    Recipe class to store recipe information
+    """
     def __init__(
         self,
         recipe_title: str,
@@ -9,14 +12,45 @@ class Recipe:
         subcategory: dict,
         image_link: str
     ) -> None:
+        """
+        Initializes a Recipe instance.
+
+        :param recipe_title: str: The title of the recipe.
+        :param recipe_link: str: The URL link to the recipe.
+        :param category: dict: The category of the recipe
+        (e.g., dessert, main course).
+        :param subcategory: dict: The subcategory of the recipe.
+        :param image_link: str: The URL link to the recipe's image.
+        """
         self.image_link: str = image_link
         self.recipe_title: str = recipe_title
         self.recipe_link: str = recipe_link
         self.category: dict = category
         self.subcategory: dict = subcategory
 
-    def __repr__(self):
-        return (f"Recipe(Title='{self.recipe_title}', Link={self.recipe_link}, "
-                f" Category={self.category}, Subcategory={self.subcategory}, "
-                f"Image={self.image_link})")
+    def to_dict(self) -> dict:
+        """
+        Converts the Recipe instance into a dictionary format.
 
+        :return: dict: A dictionary containing the recipe information.
+        """
+        return {
+            "Title": self.recipe_title,
+            "Link": self.recipe_link,
+            "Category": self.category,
+            "Subcategory": self.subcategory,
+            "Image": self.image_link,
+        }
+
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the Recipe instance.
+
+        :return: str: A string that provides a clear representation
+        of the recipe object.
+        """
+        return (
+            f"Recipe(Title='{self.recipe_title}', Link={self.recipe_link}, "
+            f" Category={self.category}, Subcategory={self.subcategory}, "
+            f"Image={self.image_link}, "
+        )
